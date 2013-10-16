d19856bc16ce32d73011c9188fee2a69deb496eb
christian.weibell <christian.weibell@192.168.1.16>
2013-10-15 23:58:31 -0600

    Spelling error correction (displayHelpBoarder -> displayHelpBorder)

--- TicTacToe_L4After/src/ticTacToe/HelpMenuControl.java
+++ TicTacToe_L4After/src/ticTacToe/HelpMenuControl.java
@@ -17,72 +17,72 @@ public class HelpMenuControl  {
 
     public void displayBoardHelp() {
         System.out.println();
-        this.displayHelpBoarder();             
+        this.displayHelpBorder();             
         System.out.println( 
                 "\tThe game board for Tic-Tac-Toe. It consist of a grid of "
                 + "\n\tlocations. Players place their marker on the different locations "
                 + "\n\ton the board in an effort to win the game. The default board is "
                 + "\n\t3 rows by 3 columns.");
-        displayHelpBoarder();
+        displayHelpBorder();
     }
     
     
         
     public void displayGameHelp() {
         System.out.println();
-        displayHelpBoarder();     
+        displayHelpBorder();     
         System.out.println( 
                  "\tThe objective of the game is to be the first player to mark three "
                 + "\n\tsquares vertically, horizontally or diagonally. Each player takes "
                 + "\n\tturns placing their marker in one of the locations on the "
                 + "\n\tboard. The first player to get \"three-in-a-row\" is the winner."
                 ); 
-        displayHelpBoarder();
+        displayHelpBorder();
     }
             
     public void displayRealPlayerHelp() {
         System.out.println();
-        displayHelpBoarder();     
+        displayHelpBorder();     
         System.out.println( 
                 "\tA real player manually takes their turn by placing their mark "
                 + "\n\tin an unused location on the board."
                 ); 
-        displayHelpBoarder();
+        displayHelpBorder();
     }
     
                    
     public void displayComputerPlayerHelp() {
         System.out.println();
-        displayHelpBoarder();     
+        displayHelpBorder();     
         System.out.println( 
                 "\tA computer based player utomatically takes its turn "
                 + "\n\timmediatly after a real player in a single player game."
                 ); 
-        displayHelpBoarder();
+        displayHelpBorder();
     }
              
     public void displayLocationHelp() {
         System.out.println();
-        displayHelpBoarder();     
+        displayHelpBorder();     
         System.out.println( 
                "\tA location on the board where a player can place their marker"
                 ); 
-        displayHelpBoarder();
+        displayHelpBorder();
     }
                  
     public void displayMarkerHelp() {
         System.out.println();
-        displayHelpBoarder();     
+        displayHelpBorder();     
         System.out.println( 
                "\tA symbol that \"marks\" the locations in the board that are occupied "
                 + "by a player. "
                 + "\n\tThe default markers are \"X\" and \"O\"."
                 ); 
-        displayHelpBoarder();
+        displayHelpBorder();
     }
     
     
-    public void displayHelpBoarder() {       
+    public void displayHelpBorder() {       
         System.out.println(
         "\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
